e8c463006dc7332f168c95af4feb285d4663099c
Karengonsan <karenglezschez@gmail.com>
2025-10-04 11:24:40 +0200

    return array paso 1

--- 5. Modulo - 3 - Arrays/src/components/App.jsx
+++ 5. Modulo - 3 - Arrays/src/components/App.jsx
@@ -1,13 +1,27 @@
-import'react'
-import '../styles/app.css'
+import "react";
+import "../styles/app.css";
 
 const App = () => {
+  const tasks = [
+    "Comprar harina, jamón y pan rallado",
+    "Hacer croquetas ricas",
+    "Ir a la puerta de un gimnasio",
+    "Comerme las croquetas mirando a la gente que entra en el gimnasio",
+  ];
 
   return (
     <>
-     
+      <h1>Mi lista de tareas</h1>
+      <ol>
+        <li>Comprar harina, jamón y pan rallado</li>
+        <li>Hacer croquetas ricas</li>
+        <li>Ir a la puerta de un gimnasio</li>
+        <li>
+          Comerme las croquetas mirando a la gente que entra en el gimnasio
+        </li>
+      </ol>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
