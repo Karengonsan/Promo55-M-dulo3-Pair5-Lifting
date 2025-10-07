d09c1e6012e6380b11623e6978634e1babdf769c
Susana.Gl <susiigarcialeon@gmail.com>
2025-10-07 21:45:15 +0200

    Clase del 07/10

--- 6. Modulo - 3 - Fetch & LocalStorage/src/components/App.jsx
+++ 6. Modulo - 3 - Fetch & LocalStorage/src/components/App.jsx
@@ -1,11 +1,45 @@
-import'react'
+import { useState, useEffect } from 'react'
 import '../styles/app.css'
 
 const App = () => {
 
+  const [series, setSeries] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  useEffect(
+  // Aquí empieza la función
+  () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${searchValue}`)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData)
+        setSeries(responseData);
+      });
+  }, [searchValue]
+  // Aquí termina la función
+);
+  const handleChange = (ev) => {
+      setSearchValue(ev.target.value);
+    }
+
   return (
     <>
-     
+
+    <h1>Busca tus series favoritas</h1>
+    <label htmlFor='name'>Busca por nombre:</label>
+    <input
+      name="name"
+      id='name'
+      value={searchValue}
+      onChange={handleChange}
+    />
+      <h3>Personajes con el nombre: {}</h3>
+    <ul>
+      {series.map (serie => {
+        return (
+          <li key={serie.show.id}>{serie.show.name}</li>
+        )
+      })}
+    </ul>
     </>
   )
 }
